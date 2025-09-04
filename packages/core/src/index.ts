74f9a5215c5adb1b318b93abd7f52bc1fb086659
doki- <1335902682@qq.com>
2025-09-04 21:28:49 +0800

    feat: 入口注册多语言

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,6 @@
 import type { App, Plugin } from 'vue';
+import type { I18n } from 'vue-i18n';
+import { createI18n } from 'vue-i18n';
 import Attachments from './components/Attachments/index.vue';
 import Bubble from './components/Bubble/index.vue';
 import BubbleList from './components/BubbleList/index.vue';
@@ -19,8 +21,23 @@ import XMarkdownAsync from './components/XMarkdownAsync/index.vue';
 export * from './components';
 export * from './hooks';
 
+type LocaleObject = Record<string, any>;
+interface ElementPlusXOptions {
+  locale: LocaleObject;
+}
+
 const ElementPlusX: Plugin = {
-  install(app: App) {
+  install(app: App, options: ElementPlusXOptions) {
+    const { locale } = options;
+    const name = locale.name;
+    const code = locale.elx;
+    const i18n = createI18n({
+      locale: name,
+      messages: {
+        [name]: code,
+      },
+    });
+    app.use(i18n);
     app.component('Attachments', Attachments);
     app.component('Bubble', Bubble);
     app.component('BubbleList', BubbleList);
---
+++ packages/core/src/locale/lang/zh-cn.ts
@@ -0,0 +1,15 @@
+export default {
+  name: 'zh-cn',
+  elx: {
+    breadcrumb: {
+      label: '面包屑',
+    },
+    colorpicker: {
+      confirm: '确定',
+      clear: '清空',
+      defaultLabel: '颜色选择器',
+      description: '当前颜色 {color}，按 Enter 键选择新颜色',
+      alphaLabel: '选择透明度的值',
+    }
+  },
+};
